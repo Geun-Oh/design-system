29d7b657140541a266b92f8d5dede9e97a0241e7
Geun-Oh <kandy1002@naver.com>
2022-08-25 07:27:56 +0000

    Component Description updated

--- storybook-app/src/stories/Input.tsx
+++ storybook-app/src/stories/Input.tsx
@@ -7,11 +7,17 @@ import { Icon, IconProps } from "./Icon";
 type InputType = "textInput" | "date" | "phoneNumber" | "password" | "searchField" | "inputWithSteper" | "textAreaInput";
 
 interface InputProps {
+    /**라이트모드와 다크모드를 설정해주세요. */
     themeType: ThemeType;
+    /**타입을 정해주세요. */
     type: InputType;
+    /**인풋의 너비를 지정해주세요. "100px", "50vw"와 같이 단위를 포함한 string으로 입력해야합니다. */
     width: string;
+    /**아이콘의 타입을 설정해주세요. */
     icon?: IconProps["type"];
+    /**인풋의 이름을 지정해주세요. 백엔드와의 통신에 제출할 폼에서 유용하게 사용됩니다. */
     name?: string;
+    /**인풋의 높이를 지정해주세요. "100px", "50vw"와 같이 단위를 포함한 string으로 입력해야합니다. */
     height?: string;
 }
 
