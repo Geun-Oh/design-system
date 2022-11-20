34edcfbde517b44157bcdc6d4e17f77717619e0a
Geun-Oh <kandy1002@naver.com>
2022-11-20 12:59:09 +0000

    added storybook maiexport Object

--- storybook-app/src/main/main.ts
+++ storybook-app/src/main/main.ts
@@ -15,3 +15,25 @@ import MarginBox from '../components/Marginbox';
 import Statusbar from '../components/Statusbar';
 import Text from '../components/Text';
 import Toggle from '../components/Toggle';
+
+const Components = {
+    Badge,
+    Box,
+    Breadcrumb,
+    Button,
+    Card,
+    Checkbox,
+    Colorpallete,
+    DropDown,
+    Flex,
+    header,
+    Icon,
+    Input,
+    LoginForm,
+    MarginBox,
+    Statusbar,
+    Text,
+    Toggle,
+}
+
+export default Components;
