7e92425366997fc607c7e7a05e6fc26239afe6b4
Geun-Oh <kandy1002@naver.com>
2023-03-13 13:44:39 +0000

    Study: refactored badge component

--- storybook-app/src/stories/Badge.stories.tsx
+++ storybook-app/src/stories/Badge.stories.tsx
@@ -1,58 +1,30 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import Badge from "../components/Badge";
+import { expect } from '@storybook/jest';
+import { waitFor, userEvent, within } from '@storybook/testing-library';
+import Example from '../components/Badge/Example';
+
+const { BadgeExample, AccessExample } = Example;
 
 export default {
-    title: 'Components/Badge',
-    component: Badge,
-    parameters: {
-        componentSubtitle: "특정 상태를 나타낼 때 사용되는 뱃지입니다."
-    }
-} as ComponentMeta<typeof Badge>;
+  title: 'Components/Badge',
+  component: BadgeExample,
+  parameters: {
+    componentSubtitle: "컴포넌트들을 감싸고 Organism의 단위가 되는 박스 컴포넌트입니다."
+  }
+} as ComponentMeta<typeof BadgeExample>;
 
-const Template: ComponentStory<typeof Badge> = (args) => <Badge {...args} />;
+const DefaultExample: ComponentStory<typeof BadgeExample> = () => <BadgeExample />;
+export const Access: ComponentStory<typeof AccessExample> = () => <AccessExample />;
 
-export const Access = Template.bind({});
-Access.args = { innerText: "Badge Innertext", state: "Access", type: "default" };
+export const Default = DefaultExample.bind({});
 
-export const Warning = Template.bind({});
-Warning.args = { innerText: "Warning Badge Innertext", state: "Warning", type: "default" };
-
-export const Error = Template.bind({});
-Error.args = { innerText: "Error Badge Innertext", state: "Error", type: "default" };
-
-export const New = Template.bind({});
-New.args = { innerText: "New Badge Innertext", state: "New", type: "default" };
-
-export const Wait = Template.bind({});
-Wait.args = { innerText: "Wait Badge Innertext", state: "Wait", type: "default" };
-
-export const TextInnerAccess = Template.bind({});
-TextInnerAccess.args = { innerText: "Badge Innertext", state: "Access", type: "textInner" };
-
-export const TextInnerWarning = Template.bind({});
-TextInnerWarning.args = { innerText: "Warning Badge Innertext", state: "Warning", type: "textInner" };
-
-export const TextInnerError = Template.bind({});
-TextInnerError.args = { innerText: "Error Badge Innertext", state: "Error", type: "textInner" };
-
-export const TextInnerNew = Template.bind({});
-TextInnerNew.args = { innerText: "New Badge Innertext", state: "New", type: "textInner" };
-
-export const TextInnerWait = Template.bind({});
-TextInnerWait.args = { innerText: "Wait Badge Innertext", state: "Wait", type: "textInner" };
-
-export const TextOverAccess = Template.bind({});
-TextOverAccess.args = { innerText: "Badge Innertext", state: "Access", type: "textOver" };
-
-export const TextOverWarning = Template.bind({});
-TextOverWarning.args = { innerText: "Warning Badge Innertext", state: "Warning", type: "textOver" };
-
-export const TextOverError = Template.bind({});
-TextOverError.args = { innerText: "Error Badge Innertext", state: "Error", type: "textOver" };
-
-export const TextOverNew = Template.bind({});
-TextOverNew.args = { innerText: "New Badge Innertext", state: "New", type: "textOver" };
-
-export const TextOverWait = Template.bind({});
-TextOverWait.args = { innerText: "Wait Badge Innertext", state: "Wait", type: "textOver" };
+Access.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  await waitFor(async () => {
+    const a = await canvas.findByRole('link');
+    expect(await canvas.findByTestId('Badge-Wrapper')).toBeInTheDocument();
+    expect(a!).toHaveAttribute("href", "https://www.naver.com/");
+    // expect((await canvas.findByTestId('Badge-Access-Wrapper')).closest("a")).toBeInTheDocument();
+  });
+};
