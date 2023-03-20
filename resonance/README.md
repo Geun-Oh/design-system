826dd23b03d34a62ad8f3899e57cbbb15ae6db19
Geun-Oh <kandy1002@naver.com>
2023-03-20 14:18:11 +0000

    fix: changed default file name storybook-app to resonance

---
+++ resonance/README.md
@@ -0,0 +1,9 @@
+## Scripts
+
+### npm run storybook
+
+- Open storybook
+
+### npm test
+
+- Run all tests in src/\_\_tests\_\_
--- storybook-app/src/components/Badge/index.ts
+++
@@ -1,5 +0,0 @@
-import BadgeWrapper from "./hooks/BadgeWrapper";
-
-const Badge = Object.assign(BadgeWrapper, {});
-
-export default Badge;
