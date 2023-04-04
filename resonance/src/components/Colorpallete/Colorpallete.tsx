248a9a4ead3355fbf14129ccf82b9e10be9865c9
Geun-Oh <kandy1002@naver.com>
2023-04-04 15:07:55 +0900

    fix: Colorpallete Object.map type error fixed

--- resonance/src/components/Colorpallete/Colorpallete.tsx
+++ resonance/src/components/Colorpallete/Colorpallete.tsx
@@ -1,9 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import React from 'react';
-import { BaseStyles } from "../../themes";
-import Pallete from './ui/Pallete';
-import MarginBox from '../Marginbox';
-import Flex from '../Flex/Flex';
+import { BaseStyles } from "@src/styles/themes";
+import Pallete from '@src/components/Colorpallete/ui/Pallete';
+import Flex from '@src/components/Flex/Flex';
 /**
  * 사용되는 컬러 팔레트입니다.
  * Black을 기반으로 포인트 색상을 프로젝트마다 다르게 적용하여 제작합니다.
