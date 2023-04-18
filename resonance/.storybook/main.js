d2878e5ace2eb9ca9594babc74f23b2cef15bef0
Geun-Oh <kandy1002@naver.com>
2023-04-18 17:59:40 +0900

    chore: storybook version upgrade

--- resonance/.storybook/main.js
+++ resonance/.storybook/main.js
@@ -1,9 +1,9 @@
-const TsconfigPathsPlugin  = require('tsconfig-paths-webpack-plugin')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 module.exports = {
   webpackFinal: async config => {
     config.module.rules.unshift({
       test: /\.svg$/,
-      use: ['@svgr/webpack'],
+      use: ['@svgr/webpack']
     });
     return {
       ...config,
@@ -14,14 +14,12 @@ module.exports = {
     };
   },
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app",
-  ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-webpack5",
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions", "@storybook/preset-create-react-app", "@storybook/addon-mdx-gfm"],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
   },
+  docs: {
+    autodocs: true
+  }
 };
